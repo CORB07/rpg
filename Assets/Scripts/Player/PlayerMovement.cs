66b1e3beab5a7b7809f25a8685364880bcacdc93
Stade <stadec@students.dsbn.org>
2025-03-27 12:52:57 -0400

    added screen change

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
-
     private Vector2 _movement;
 
     private Rigidbody2D _rb;
--- Assets/Scripts/moveCamera.cs
+++ Assets/Scripts/moveCamera.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class moveCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
+    public float _newcamposx = 0;
+    public float _newcamposy = 0;
+    public float _newcamposz = -10;
+
+
+
     /*private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject otherObj = collision.gameObject;
         Debug.Log("Collided with: " + otherObj); 
     }*/
 
+    private void Start()
+    {
+        Camera.main.transform.position = new Vector3(0, 0, -10);
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         //TODO: make sure it triggers with PLAYER only
         GameObject otherObj = collider.gameObject;
         Debug.Log("Triggered with: " + otherObj);
+
+        Camera.main.transform.position = new Vector3(_newcamposx, _newcamposy, _newcamposz);
     }
 }
